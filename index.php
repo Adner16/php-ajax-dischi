993e0627415e038b9b740cf7c00b263e199c692f
Adner16 <giovanni.renda16@gmail.com>
2022-03-01 17:17:39 +0100

    added style

--- index.php
+++ index.php
@@ -1,55 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Disc</title>
+</head>
+<body>
+    <header class='d-flex align-items-center'>
+        <div class='container-fluid d-flex align-items-center'>
+            <div class='logo-container'>
+                <img class='img-fluid logo' src="https://upload.wikimedia.org/wikipedia/commons/7/75/Spotify_icon.png" alt="">
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class='container'>
+            <div class='card'>
+                <div class='image'>
+                    <img src="<?php $disc['poster']?>" alt="figure">
+                </div>    
+                    <h3 class='title'><?php echo $disc['title'] ?></h3>
+                    <span class='artist grey'><?php echo $disc['author'] ?></span><br>
+                    <span class='year grey'><?php echo $disc['year'] ?></span> 
+            </div>
+        </div>
+        
+    </main>
 <?php
+    include 'data.php';
 
-    $database = [
-        [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
-            'genre' => 'Rock'
-        ],
-        [
-            'title' => 'Live at Wembley 86',
-            'author' => 'Queen',
-            'year' => 1992,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Ten\'s Summoner\'s Tales',
-            'author' => 'Sting',
-            'year' => 1993,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Steve Gadd band',
-            'author' => 'Steve Gadd Band',
-            'year' => 2018,
-            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
-            'genre' => 'Jazz'
-        ],
-        [
-            'title' => 'Brave new World',
-            'author' => 'Iron Maiden',
-            'year' => 2000,
-            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
-            'genre' => 'Metal'
-        ],
-        [
-            'title' => 'One more car, one more rider',
-            'author' => 'Eric Clapton',
-            'year' => 2002,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
-            'genre' => 'Rock'
-        ]
-    ];
+    // array_keys($database);
+
+    // foreach($database as $disc){
+    //     echo $disc['title'] . '<br>';
+    //     echo $disc['author'] . '<br>';
+    //     echo $disc['year'] . '<br>';
+    //     echo '<img src="' . $disc['poster'] . '" > <br>';
+    //     echo $disc['genre'] . '<br>';
+        
+    // }
+?>
+</body>
+</html>
 
 
-    array_keys($database);
 
-    foreach($database as $disc){
-        foreach($disc as $key => $value){
-            echo $value;
-        }
-    }
